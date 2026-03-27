39faaa91b09f63908669b01fba0c6de8baf7a97a
Itallo Polito <itallomp@hotmail.com>
2026-03-27 13:47:51 -0300

    Update Deploy Render

--- security.py
+++ security.py
@@ -3,44 +3,47 @@ from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from config import settings
 
-
-pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
+
+ALGORITHM = "HS256"
+
 
 def hash_senha(senha: str):
     return pwd_context.hash(senha)
-    
+
+
 def verificar_senha(senha, hash):
     return pwd_context.verify(senha, hash)
 
+
 def criar_token(usuario_id: int):
     payload = {
-        "sub" : str(usuario_id),
+        "sub": str(usuario_id),
         "exp": datetime.now(timezone.utc) + timedelta(hours=1)
     }
-    
-    return jwt.encode(payload, "KEY", algorithm="HS256")
+
+    return jwt.encode(payload, settings.secret_key, algorithm=ALGORITHM)
+
 
 def verificar_token(token: str):
     try:
-        payload = jwt.decode(token, "KEY", algorithms=["HS256"])
-        
+        payload = jwt.decode(token, settings.secret_key, algorithms=[ALGORITHM])
+
         user_id = payload.get("sub")
-        
+
         if user_id is None:
             raise HTTPException(status_code=401, detail="Token inválido")
-        
+
         return int(user_id)
-    
+
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido")
-    
+
 
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
-    
     user_id = verificar_token(token)
-    
     return user_id
